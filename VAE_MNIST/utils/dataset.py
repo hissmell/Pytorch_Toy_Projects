f78f704a91cc7e1478d1918f9010bec6e4082dac
ParkWonGyu <pn50212@hanyang.ac.kr>
2022-01-25 21:24:39 +0900

    load_mnist_dataset function fin!

--- VAE_MNIST/utils/dataset.py
+++ VAE_MNIST/utils/dataset.py
@@ -1,22 +1,28 @@
 from torchvision.transforms import ToTensor, Resize, Normalize, Compose
-from torchvision.datasets import ImageFolder
+from torchvision.datasets import ImageFolder,MNIST
 from torch.utils.data import DataLoader, Dataset
 
 import os
 
-
-def load_image_dataset(data_dir_path=None, image_size=(64, 64), means=(0.5, 0.5, 0.5), stds=(0.5, 0.5, 0.5),
-                       batch_size=64, num_workers=1):
-    if type(image_size) == int:
-        image_size = (image_size, image_size)
-    if data_dir_path == None:
-        data_dir_path = os.getcwd()
-
-    image_transformation = Compose([Resize(image_size), ToTensor(), Normalize(means, stds)])
-    train_dataset = ImageFolder(os.path.join(data_dir_path, 'train'), transform=image_transformation)
-    valid_dataset = ImageFolder(os.path.join(data_dir_path, 'valid'), transform=image_transformation)
-
+def load_mnist_dataset(means=(0.5), stds=(0.5),batch_size=64, num_workers=0):
+    image_transformation = Compose([ToTensor(), Normalize(means, stds)])
+    train_dataset = MNIST(root='C:\\Users\\82102\\PycharmProjects\\ReinforcementLearning\\Toy_project\\VAE_MNIST\\data'
+                          ,train=True
+                          ,transform=image_transformation
+                          ,download=True)
+    valid_dataset = MNIST(root='C:\\Users\\82102\\PycharmProjects\\ReinforcementLearning\\Toy_project\\VAE_MNIST\\data'
+                          ,train=False
+                          ,transform=image_transformation
+                          ,download=True)
     train_data_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
     valid_data_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
 
-    return train_data_loader, valid_data_loader
+    return train_data_loader,valid_data_loader
+
+if __name__ == '__main__':
+    train_data_loader,valid_data_loader = load_mnist_dataset()
+    images,labels = next(iter(train_data_loader))
+    print(images.size())
+    print(images.dtype)
+    print(labels.size())
+    print(labels.dtype)

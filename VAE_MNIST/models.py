383fc4e209f60f2a67105a29cad841224c70d2a8
ParkWonGyu <pn50212@hanyang.ac.kr>
2022-01-26 12:13:15 +0900

    model.py fin

--- VAE_MNIST/models.py
+++ VAE_MNIST/models.py
@@ -1,14 +1,75 @@
 import torch
-from torch.nn import Module,Linear,ReLU
+from torch.nn import Module,Linear,ReLU,Conv2d,MaxPool2d,Flatten,ConvTranspose2d
 
-class TestModel(Module):
+class Encoder(Module):
     def __init__(self):
-        super(TestModel, self).__init__()
-        self.dense = Linear(10,10)
-        self.relu = ReLU()
+        super(Encoder, self).__init__()
+        self.conv1 = Conv2d(1,64,kernel_size=(3,3),stride=(1,1),padding=(1,1),dtype=torch.float32)
+        self.maxpool1 = MaxPool2d(kernel_size=(2,2),stride=(2,2))
+        self.relu1 = ReLU()
+        self.conv2 = Conv2d(64,64,kernel_size=(3,3),stride=(1,1),padding=(1,1),dtype=torch.float32)
+        self.maxpool2 = MaxPool2d(kernel_size=(2,2),stride=(2,2))
+        self.relu2 = ReLU()
+        self.conv3 = Conv2d(64,64,kernel_size=(3,3),stride=(1,1),padding=(1,1),dtype=torch.float32)
+        self.relu3 = ReLU()
+        self.flatten = Flatten(start_dim=1)
+        self.mean = Linear(3136,3,dtype=torch.float32)
+        self.log_variance = Linear(3136,3,dtype=torch.float32)
 
-    def forward(self,inputs):
-        inputs = self.dense(inputs)
-        inputs = self.relu(inputs)
-        return inputs
+    def forward(self,x):
+        x = self.relu1(self.maxpool1(self.conv1(x)))
+        x = self.relu2(self.maxpool2(self.conv2(x)))
+        x = self.relu3(self.conv3(x))
+        x = self.flatten(x)
+        means = self.mean(x)
+        variances = torch.exp(self.log_variance(x))
+        return means,variances
 
+class Sampler(Module):
+    def __init__(self):
+        super(Sampler, self).__init__()
+
+    def forward(self,means,variances):
+        batch_size = means.size()[0]
+        z = variances * torch.normal(0,1,size=(batch_size,3)) + means
+        return z
+
+class Decoder(Module):
+    def __init__(self):
+        super(Decoder, self).__init__()
+        self.dense = Linear(3,3136,dtype=torch.float32)
+        self.relu1 = ReLU()
+        self.deconv1 = ConvTranspose2d(64,64,kernel_size=(4,4),stride=(2,2),padding=(1,1),dtype=torch.float32)
+        self.relu2 = ReLU()
+        self.deconv2 = ConvTranspose2d(64, 1, kernel_size=(4,4), stride=(2,2), padding=(1,1), dtype=torch.float32)
+
+    def forward(self,x):
+        x = self.relu1(self.dense(x))
+        x = x.view(-1,64,7,7)
+        x = self.relu2(self.deconv1(x))
+        x = self.deconv2(x)
+        return x
+
+class VAE(Module):
+    def __init__(self):
+        super(VAE, self).__init__()
+        self.encoder = Encoder()
+        self.sampler = Sampler()
+        self.decoder = Decoder()
+
+    def forward(self,x):
+        means,variances = self.encoder(x)
+        z = self.sampler(means,variances)
+        output = self.decoder(z)
+        return output
+
+
+
+if __name__ =='__main__':
+    import numpy as np
+    test_input = torch.from_numpy(np.random.randn(64,1,28,28).astype(np.float32))
+    model = VAE()
+    test_ouput = model(test_input)
+    print(test_ouput.shape)
+    print(test_ouput.dtype)
+    print(model)

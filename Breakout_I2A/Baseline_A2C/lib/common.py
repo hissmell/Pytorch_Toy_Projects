8c40d91f9710000ad5021a36b0b109d81fbc710b
ParkWonGyu <pn50212@hanyang.ac.kr>
2022-02-20 22:44:55 +0900

    20220220 last check

--- Breakout_I2A/Baseline_A2C/lib/common.py
+++ Breakout_I2A/Baseline_A2C/lib/common.py
@@ -16,24 +16,32 @@ REWARD_STEPS = 5
 NUM_ENVS = 50
 GAMMA = 0.99
 ENTROPY_BETA = 0.01
-CLIP_GRAD = 0.1
+CLIP_GRAD = 0.5
 STOP_REWARD = 400
-MOVING_AVG_WIDTH = 10
+MOVING_AVG_WIDTH = 100
+BATCH_SIZE = 128
 
 ''' FUNCTIONS '''
 
-def set_save_path():
+def set_save_path(RUN_NAME):
     save_path = os.path.join(os.getcwd(),'save')
+    os.makedirs(save_path,exist_ok=True)
+    save_path = os.path.join(os.getcwd(),'save',RUN_NAME)
     os.makedirs(save_path,exist_ok=True)
     return save_path
 
-def make_env():
+def make_env(test=False, clip=True):
     '''
     This function returns a list of environments (length : NUM_ENVS)
     '''
-    return [ptan.common.wrappers.wrap_dqn(gym.make(ENV_NAME),stack_frames=STACK_FRAMES) for _ in range(NUM_ENVS)]
+    if test:
+        args = {'reward_clipping': False,
+                'episodic_life': False}
+    else:
+        args = {'reward_clipping': clip}
+    return [ptan.common.wrappers.wrap_dqn(gym.make(ENV_NAME),stack_frames=STACK_FRAMES,**args) for _ in range(NUM_ENVS)]
 
-def unpach_batch(batch,net,device):
+def unpack_batch(batch,net,device):
     '''
     This function works to make batch to trainable-state
     :param batch:
@@ -47,11 +55,11 @@ def unpach_batch(batch,net,device):
     not_done_idx = []
     last_states = []
 
-    for idx,exp in range(batch):
+    for idx,exp in enumerate(batch):
         states.append(np.array(exp.state,copy=False))
         actions.append(exp.action)
         rewards.append(exp.reward)
-        if not exp.done:
+        if exp.last_state is not None:
             not_done_idx.append(idx)
             last_states.append(np.array(exp.last_state,copy=False))
 
@@ -60,30 +68,78 @@ def unpach_batch(batch,net,device):
     rewards_np = np.array(rewards,dtype=np.float32)
 
     if not_done_idx:
-        last_states_var = torch.tensor(np.array(last_states),dtype=torch.float32)
+        last_states_var = torch.tensor(np.array(last_states),dtype=torch.float32).to(device)
         values_var = net(last_states_var)[1][:,0] # to 1-D array
         rewards_np[not_done_idx] += values_var.data.to('cpu').numpy() * (GAMMA ** REWARD_STEPS)
 
     value_refer_var = torch.tensor(rewards_np,dtype=torch.float32).to(device)
     return states_var,actions_var,value_refer_var
 
+def train(net,optimizer,batch,tb_tracker,frame,device):
+    states_var,actions_var,value_refer_var = unpack_batch(batch,net,device)
+    batch.clear()
+
+    optimizer.zero_grad()
+    logits_var,values_var = net(states_var)
+
+    loss_value = F.mse_loss(values_var.squeeze(-1),value_refer_var)
+
+    log_prob_var = F.log_softmax(logits_var,dim=1)
+    advance_var = value_refer_var - values_var.squeeze(1).detach()
+    log_prob_action_var = log_prob_var[range(BATCH_SIZE),actions_var]
+    loss_policy = advance_var.detach() * log_prob_action_var
+    loss_policy = -loss_policy.mean()
+
+    prob_var = F.softmax(logits_var,dim=1)
+    loss_entropy = log_prob_var * prob_var
+    loss_entropy = ENTROPY_BETA * loss_entropy.sum(dim=1).mean()
+
+    loss_value_policy = loss_policy + loss_value
+    loss_value_policy.backward(retain_graph=True)
+    grads = np.concatenate([p.grad.data.to('cpu').numpy().flatten() for p in net.parameters() if p.grad is not None])
+
+    loss_entropy.backward()
+    nn.utils.clip_grad_norm_(net.parameters(),CLIP_GRAD)
+
+    optimizer.step()
+    loss_total = loss_value_policy + loss_entropy
+
+    ''' Record Training Data '''
+    tb_tracker.track('advantage', advance_var.detach().mean().to('cpu'), frame)
+    tb_tracker.track('values', values_var.detach().mean().to('cpu'), frame)
+    tb_tracker.track('batch_rewards', value_refer_var.detach().mean().to('cpu'), frame)
+    tb_tracker.track('loss_entropy', loss_entropy.detach().to('cpu'), frame)
+    tb_tracker.track('loss_policy', loss_policy.detach().to('cpu'), frame)
+    tb_tracker.track('loss_value', loss_value.detach().to('cpu'), frame)
+    tb_tracker.track('loss_total', loss_total.detach().to('cpu'), frame)
+    tb_tracker.track('grad_l2', np.sqrt(np.mean(np.square(grads))), frame)
+    tb_tracker.track('grad_max', np.max(np.abs(grads)), frame)
+    tb_tracker.track('grad_var', np.var(grads), frame)
+
+
 ''' CLASSES '''
 class RewardTracker:
     '''
     It used as "with RewardTracker(writer)"
     '''
-    def __init__(self,writer):
+    def __init__(self,writer,save_offset = 5.0):
         self.writer = writer
         self.stop_reward = STOP_REWARD
         self.moving_avg_width = MOVING_AVG_WIDTH
+        self.best_reward_MA = 0.0
+        self.save_offset = save_offset
 
     def __enter__(self):
         self.ts = time.time()
         self.ts_frame = 0.
         self.scores = []
+        return self
 
-    def check_score(self,score,frame,epsilon=None):
-        self.scores = []
+    def __exit__(self,*args,**kwargs):
+        self.writer.close()
+
+    def check_score(self,score,frame,epsilon=None,net=None,save_dir=None,RUN_NAME=None):
+        self.scores.append(score)
         speed = (frame - self.ts_frame) / (time.time() - self.ts)
         self.ts = time.time()
         self.ts_frame = frame
@@ -98,7 +154,13 @@ class RewardTracker:
         self.writer.add_scalar('speed', speed, frame)
         self.writer.add_scalar('score', score, frame)
 
+        if moving_avg > self.best_reward_MA + self.save_offset:
+            save_to = os.path.join(save_dir, RUN_NAME + f"-frame={frame}-score={moving_avg:.6f}-pth")
+            torch.save(net.state_dict(),save_to)
+
         if moving_avg > self.stop_reward:
             print(colored(f"Solved in {len(self.scores)} Games!"), 'yellow')
+            save_to = os.path.join(save_dir, RUN_NAME + f"-frame={frame}-solved-pth")
+            torch.save(net.state_dict(), save_to)
             return True
         return False
--- Breakout_I2A/Baseline_A2C/train_a2c.py
+++ Breakout_I2A/Baseline_A2C/train_a2c.py
@@ -1,11 +1,47 @@
 import torch
 import numpy as np
 import ptan
-
+import torch.optim as optim
+from lib import common
+from lib import models
+from tensorboardX import SummaryWriter
+import os
 
 ''' SETTING '''
 RUN_NAME = 'Exp_01'
 LEARNING_RATE = 1e-4
-
+SAVE_OFFSET = 5.0
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 ''' MAIN '''
+if __name__ == '__main__':
+    save_dir = common.set_save_path(RUN_NAME)
+
+    envs = common.make_env()
+    writer = SummaryWriter(comment=f'-lr={LEARNING_RATE:8.6f}')
+
+    net = models.A2C(envs[0].observation_space.shape,envs[0].action_space.n).to(DEVICE)
+    print(net)
+
+    agent = ptan.agent.PolicyAgent(lambda x: net(x)[0],apply_softmax=True,device=DEVICE)
+    exp_source = ptan.experience.ExperienceSourceFirstLast(envs,agent,common.GAMMA,common.REWARD_STEPS)
+
+    optimizer = optim.Adam(net.parameters(),lr=LEARNING_RATE,eps=1e-4)
+
+    buffer = []
+    with common.RewardTracker(writer,SAVE_OFFSET) as reward_tracker:
+        with ptan.common.utils.TBMeanTracker(writer,batch_size=100) as tb_tracker:
+            for frame, exp in enumerate(exp_source):
+                buffer.append(exp)
+
+                # handle new reward
+                new_rewards = exp_source.pop_total_rewards()
+                if new_rewards:
+                    if reward_tracker.check_score(new_rewards[0],frame,net=net,save_dir=save_dir,RUN_NAME=RUN_NAME):
+                        break
+
+                if len(buffer) < common.BATCH_SIZE:
+                    continue
+
+                common.train(net,optimizer,buffer,tb_tracker,frame,DEVICE)
+
